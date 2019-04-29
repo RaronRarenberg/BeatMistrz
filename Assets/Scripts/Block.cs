fb3551a607cdf656ca68780379fd0b4168382623
RaronRarenberg <43893501+RaronRarenberg@users.noreply.github.com>
2019-04-29 19:18:43 +0200

    First few notes work

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -13,6 +13,6 @@ public class Block : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0f,-0.1f,0f);
+        transform.Translate(0f,-13f * Time.deltaTime,0f);
     }
 }
