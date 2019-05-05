4097607833e16212d43b2d81b299d038f9dca233
RaronRarenberg <43893501+RaronRarenberg@users.noreply.github.com>
2019-05-05 18:11:06 +0200

    Narzędzie do przyśpieszania i spowalniania gry

---
+++ Assets/MusicSpeed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSpeed : MonoBehaviour
+{
+    AudioSource music;
+    // Start is called before the first frame update
+    void Start()
+    {
+        music = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (music.pitch == 1.0f)
+            {
+                music.pitch = 0.5f;
+            }
+            else
+                music.pitch = 1.0f;
+        }
+    }
+}
