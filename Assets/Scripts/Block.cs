21d147db904b6e2340fae58d1b8016286959a71e
RaronRarenberg <43893501+RaronRarenberg@users.noreply.github.com>
2019-05-30 23:42:15 +0200

    score and highscore working

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Block : MonoBehaviour
 {
+    Points scoore;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoore = GameObject.Find("points").GetComponent<Points>();
     }
-
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        scoore.number = scoore.number + 1;
+    }
+        // Update is called once per frame
+        void Update()
     {
         transform.Translate(0f,-13f * Time.deltaTime,0f);
     }
---
+++ Assets/Scripts/Points.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Points : MonoBehaviour
+{
+    public Text score;
+    public int number;
+    public Text highscore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score.text = number.ToString();
+        highscore.text = PlayerPrefs.GetInt("HighScore", 0).ToString();
+    }
+    void Update()
+    {
+        score.text = number.ToString();
+        if (number > PlayerPrefs.GetInt("HighScore", 0))
+        {
+            PlayerPrefs.SetInt("HighScore", number);
+        }
+    }
+}
