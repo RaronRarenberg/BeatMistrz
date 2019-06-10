5f0302f02c1b3a26bd00aea7d50bb6d1124c0418
RaronRarenberg <43893501+RaronRarenberg@users.noreply.github.com>
2019-06-10 16:48:37 +0200

    10.06 Update
    All buttons have hover tint and sounds with master  volume affecting those sound effects

---
+++ Assets/Scripts/OnMouseOverButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class OnMouseOverButton : MonoBehaviour, IPointerEnterHandler
+{
+    AudioSource music ;
+    void Start()
+    {
+        music = GetComponent<AudioSource>();
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        music.volume = PlayerPrefs.GetFloat("MasterVolume", 1);
+        music.Play();
+
+    }
+}
